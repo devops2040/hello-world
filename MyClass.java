baf6c5ed45505246f79568404611ba84184f1404
devops2040 <41792125+devops2040@users.noreply.github.com>
2018-08-01 20:17:12 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -2,7 +2,7 @@ public class MyClass{
 
 	public static void main(String[] args){
 		System.out.println("Hello World");
-		System.out.println("ContiousIntegration");
+		System.out.println("ContinousIntegration");
 	}
 
 
